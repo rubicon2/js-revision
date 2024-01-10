d25a735d31afda9387e34a02c331adf7e98bed25
rubicon2 <liargent@hotmail.com>
2024-01-10 17:23:04 +0000

    Finish lesson factory functions and the module pattern

--- script.js
+++ script.js
@@ -103,3 +103,50 @@ console.log('v2: ', v2);
 [v1, v2] = [v2, v1];
 console.log('v1: ', v1);
 console.log('v2: ', v2);
+
+// How scope works in JS
+// var is function scoped. If declared outside a function, will be 'global'
+// const, let are block scoped, i.e. accessible until the { block } they are declared in is closed. If declared outside of any blocks, will be 'global'
+
+// What are closures?
+// TO DO: try to explain the entirely of closures and their usefulness
+// objects 'enclosed' by a function and not returned can be used like private variables, methods, etc. 
+
+// Problems with constructors
+// People forgetting the 'new' keyword. Strange syntax (it's just a normal function but when you invoke it, you have to use the 'new' keyword). 
+// If functions declared on individual objects and not prototype, uses a lot more memory. 
+// instanceof does not work properly, prototype of an object can be changed after it has been created
+// also returns true for any prototypes in chain, not just the last one i.e. the function used to create said object
+console.log(bill instanceof Person);
+// Enemy type inherits from Person
+console.log(bill instanceof Enemy);
+// Person type inherits from Object
+console.log(bill instanceof Object);
+
+// Describe private variables in factory functions and how they can be useful
+// Create variables in factory function and then do not return as part of the object. Closure! The unreturned variables are 'enclosed' by the function that created the object.
+// Stop polluting global namespace, encapsulate data for object and expose only what needs to be exposed.
+// I.e. any variables used for the implementation of that object but not relevant to rest of code - encapsulate and hide away.
+
+// Describe how we can implement prototypal inheritance with factory functions
+// As above, use factory functions and Object.assign, i.e. object composition
+// So one factory function will call another factory function, and then sellotape on some extra stuff, basically. 
+
+// Describe immediately invoked function expressions (IIFEs)
+// Used to implement module pattern. Using closure, hide away any variables/methods that aren't properties on the returned object.
+
+// Explain the concept of namespacing and how factory functions help with encapsulation
+// Namespacing, i.e. keeping groups of functions in separate and logical groups to avoid conflicts between similarly named functions.
+// E.g. a math module that keeps functions like random
+// How about a character module that creates characters and also has a function called random?
+// Without namespacing, a call to random calls whichever was declared last. 
+// With functions defined in various files, the result may not be as predictable as below. Possibly down to import order? 
+function noNamespace() {
+    return 0;
+}
+
+function noNamespace() {
+    return 1;
+}
+
+alert(noNamespace());
